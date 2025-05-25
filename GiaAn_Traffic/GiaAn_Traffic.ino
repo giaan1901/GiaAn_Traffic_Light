18625bcd3a9fc035da66820d9b61135854948e55
giaan1901 <90020583+giaan1901@users.noreply.github.com>
2025-05-25 21:37:32 +0700

    Update GiaAn_Traffic.ino

--- GiaAn_Traffic/GiaAn_Traffic.ino
+++ GiaAn_Traffic/GiaAn_Traffic.ino
@@ -2,14 +2,20 @@
 #include "GiaAnTraffic.h"
 
 GiaAnTraffic traffic(3, 2, 2);
-const int buttonPin = 2;
+const int WalkingButton = 2;
+const int HandButton = 3;
+const int SwitchButton = 4;
+const int testLed = 5;
 int buttonState = 0;
 
 void setup() {
   
   //ButtonPin
-  pinMode(buttonPin, INPUT);
-  pinMode(7, OUTPUT);
+  pinMode(WalkingButton, INPUT);
+  pinMode(HandButton, INPUT);
+  pinMode(SwitchButton, INPUT);
+  pinMode(testLed, OUTPUT);
+  
   //Traffic Light
      
   traffic.setPin(8, 9, 10);
@@ -18,13 +24,30 @@ void setup() {
   traffic.setup();
 }
 
+int walking_cnt = 0;
+int hand_cnt = 0;
+int switch_cnt = 0;
+
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  
-  if (buttonState == HIGH) {
-    traffic.NormalState();
-    traffic.WalkingState();
-  } else {
-    traffic.NormalState();
+  int WalkingState = digitalRead(WalkingButton);
+  int HandState = digitalRead(HandButton);
+  int SwitchState = digitalRead(SwitchButton);
+
+  walking_cnt += WalkingState;
+  hand_cnt += HandState;
+  switch_cnt += SwitchState;
+
+  switch (switch_cnt & 1) {
+    case 0:
+      if (walking_cnt & 1) {
+        traffic.WalkingState();
+      } else {
+        traffic.NormalState();
+      } 
+      break;
+    case 1:
+      digitalWrite(testLed, HIGH);
+      traffic.HandState(hand_cnt);
+      break;
   }
 }
